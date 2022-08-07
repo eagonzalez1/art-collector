616bdb1fd88120f23120feddf4983ceaa006966e
eagonzalez81 <gonzoatc81@gmail.com>
2022-08-07 16:35:15 -0400

    create art and artist models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Artist(models.Model):
+  name = models.CharField(max_length=100)
+  about = models.TextField(max_length=500)
+
+  def __str__(self):
+    return self.name
+
+class Art(models.Model):
+  title = models.CharField(max_length=100)
+  date = models.DateField('Completion Date')
+  description = models.TextField(max_length=500)
+  artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.name
