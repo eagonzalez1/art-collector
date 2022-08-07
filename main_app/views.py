4320d15f23f8a082012df7c50f590609f51eec40
eagonzalez81 <gonzoatc81@gmail.com>
2022-08-07 16:24:02 -0400

    create home page

--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 
 def home(request):
-  return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
-
 
 def arts_index(request):
   return render(request, 'arts/index.html', { 'arts': arts })
