a4c700487a7a56b3568d36aba4d85184184f0ea0
eagonzalez81 <gonzoatc81@gmail.com>
2022-08-09 11:31:33 -0400

    add photo

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 # import your models here
-from .models import Art, Artist
+from .models import Art, Artist, Photo
 
 # Register your models here
 admin.site.register(Art)
 admin.site.register(Artist)
+admin.site.register(Photo)
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Art, Artist
+from .models import Art, Artist, Photo
+import uuid
+import boto3
 
 
 def home(request):
@@ -30,3 +32,33 @@ class ArtUpdate(UpdateView):
 class ArtDelete(DeleteView):
   model = Art
   success_url = '/Arts/'
+
+
+
+S3_BASE_URL = 'https://s3.us-east-1.amazonaws.com/'
+BUCKET = 'marich-art-collecotr-app'
+
+def add_photo(request, art_id):
+  # photo-file will be the "name" attribute on the <input type="file">
+  photo_file = request.FILES.get('photo-file', None)
+  if photo_file:
+    s3 = boto3.client('s3')
+    # need a unique "key" for S3 / needs image file extension too
+		# uuid.uuid4().hex generates a random hexadecimal Universally Unique Identifier
+    # Add on the file extension using photo_file.name[photo_file.name.rfind('.'):]
+    key = uuid.uuid4().hex + photo_file.name[photo_file.name.rfind('.'):]
+    # just in case something goes wrong
+    try:
+      s3.upload_fileobj(photo_file, BUCKET, key)
+      # build the full url string
+      url = f"{S3_BASE_URL}{BUCKET}/{key}"
+      # we can assign to art_id or art (if you have a art object)
+      photo = Photo(url=url, art_id=art_id)
+      # Remove old photo if it exists
+      art_photo = Photo.objects.filter(art_id=art_id)
+      if art_photo.first():
+        art_photo.first().delete()
+      photo.save()
+    except Exception as err:
+      print('An error occurred uploading file to S3: %s' % err)
+  return redirect('arts_detail', art_id=art_id)
